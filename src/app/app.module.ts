3d1f66053b8fec5bdad384ce3ffed84e90514b17
sg1asgard <sg1asgard@outlook.com>
2020-04-18 12:21:10 +0300

    Updates setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
+import { RestaurantMenuComponent } from './components/restaurant-menu/restaurant-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantComponent,
+    RestaurantMenuComponent,
   ],
   imports: [
     BrowserModule,
